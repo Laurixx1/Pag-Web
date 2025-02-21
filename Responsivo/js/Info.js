37ebdda7673947d8a6841c042e3573444de2f88a
Laurixx1 <laurigaleano2001@gmail.com>
2025-02-21 10:07:48 -0500

    commit

---
+++ Responsivo/js/Info.js
@@ -0,0 +1,46 @@
+// Comentario( // ), Variables: let, var
+let nombre = "Eder Lara T";
+let Nombre = 'Roger, ';
+let nomBre = `Julieth`;
+// Cómo debemos escribir variables:
+// Forma pascal Case: Nombre
+// Forma camell Case: nombreCompleto, nombre
+// Forma snake Case : nombre_completo
+
+// Forma de escribir:
+// Variables = snake_case
+// Funciones = cammelCase
+// Clases = PascalCase
+
+// Constantes:
+const DATABASE = 'MySQL';
+
+
+//console.log(nombre, Nombre, nomBre, DATABASE);
+
+// Tipos de Datos:
+// Tipos de datos primitivos (números, cadenas, booleanos, null, undefined)
+
+let numero = 123
+let enteros = 123         // int = 32,63 y piquito
+let flotantes = 234.23    // float = 
+let texto = `Esto es un texto @!"#$$%&//(976487.23123"`
+let booleano = true || false
+let dato_null = null;
+let dato_indefinido;
+
+dato_indefinido = `Hola Porque estas sola, sola te veo pasar...`
+console.log(enteros, flotantes, texto, dato_indefinido )
+dato_indefinido = 123.32
+
+console.log(enteros, flotantes, texto, dato_indefinido )
+
+// Operadores:
+// Operadores aritméticos (+, -, *, /, **, //, %)
+
+let sumando = 1
+let otro_sumando = 6
+let suma = sumando + otro_sumando;
+console.log(suma);
+let variable_texto = 'hola' + 'Por qué estás sola?' 
+console.log(variable_texto)
