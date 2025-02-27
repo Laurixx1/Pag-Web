9067e775b00d4d6dd3c2a3244164d8293e6d5dd9
Laura Galeano Ruiz <laurigaleano2001@gmail.com>
2025-02-26 21:48:29 -0500

    Create login.js

---
+++ Responsivo/js/login.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function loginUsuatio(){
+
+  let email = document.getElementById('email').value:
+  let password = document.getElementById('password').value;
+
+  alert('iniciando sesión');
+
+  window.location.href =
+}
